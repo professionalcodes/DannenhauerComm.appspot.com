05ebb08411bcd2351f7b28414e29be4cfe1f7620
Tucker Dannenhauer <Tucker Dannenhauer>
2016-11-30 16:24:46 -0800

    Added Angular Method Signatures for the first social media login

--- static/js/ng-controllers.js
+++ static/js/ng-controllers.js
@@ -2,7 +2,24 @@ var dannenhauerComm = dannenhauerComm || {};
 var controllers = dannenhauerComm.controllers = angular.module('dannenhauerCommControllers', ['ngRoute']);
 
 controllers.controller('RootCtrl', ['$scope', '$location', function ($scope, $location) {
+  
+
+}])
+
+controller('SocialMediaLoginCtrl', ['$scope', function ($scope) {
     
+    $scope.google = function (argument) {
+        // body...
+    };
+
+    $scope.facebook = function (argument) {
+        // body...
+    };
+
+    $scope.github = function (argument) {
+        // body...
+    };
+
 }])
 
 
